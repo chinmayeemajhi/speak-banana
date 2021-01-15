37d21a1bade7565aa99227c16ade804f3c1dde5c
chinmayeemajhi <60913313+chinmayeemajhi@users.noreply.github.com>
2021-01-15 20:14:49 +0530

    added features

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+// var que1 = document.querySelector("textarea");
+// console.log(que1);
+// var que2 = document.querySelector(".btn-primary");
+// console.log(que2);
+// var que3 = document.querySelector("[name=translator]");
+// console.log(que3);
+
+//console.log(txtInput);
+
+
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+var urlServer = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+//var urlServer = "";
+
+function constructUrl(text) {
+    var url = urlServer + "?" + "text=" + text;
+    return url;
+};
+
+function errorHandler(error) {
+
+    console.log("error occured " + error);
+};
 
 function clickEventHandler() {
-    console.log(txtInput.value);
-    console.log("clicked!");
-};  
+    var inputText = txtInput.value;//taking input
+
+    //processing input
+    fetch(constructUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;//showing output
+        })
+        .catch(errorHandler)//handling error 
+
+};
 
 btnTranslate.addEventListener("click", clickEventHandler);
